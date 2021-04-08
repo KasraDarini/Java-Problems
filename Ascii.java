860aa20442b22e907205632acfd37817d51926cc
Mohammad Kasra Darini <71825544+KasraDarini@users.noreply.github.com>
2021-04-08 17:15:41 +0430

    Add files via upload
    A simple program to print a String value as an Ascii code .

---
+++ Ascii.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Ascii {
+
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        char a = scan.next().charAt(0);
+        int b = (int) a;
+        System.out.println(b);
+
+    }
+}
