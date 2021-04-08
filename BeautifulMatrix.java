7539e190cdebafa7f7459d5f4936d27d0f7c8432
Mohammad Kasra Darini <71825544+KasraDarini@users.noreply.github.com>
2021-04-08 23:41:40 +0430

    Add files via upload

---
+++ BeautifulMatrix.java
@@ -0,0 +1,32 @@
+import java.util.*  ;
+public class BeautifulMatrix{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in) ;
+        int [][] mtx =new int [5][5] ;
+        for(int i= 0 ; i < 5 ; i++ ){
+            for(int j = 0 ; j <5 ; j++){
+
+                mtx[i][j] =sc.nextInt();
+            }
+
+        }
+
+        int j2 ;
+        int i2  ;
+        int beautifulness =0;
+        for(int i=0 ; i < 5 ; i++){
+
+            for(int j = 0 ; j < 5 ; j++){
+                    if(mtx[i][j] != 0){
+                         j2 =Math.abs( 2 -j);
+                         i2 =Math.abs(2-i) ;
+
+                         beautifulness =i2 + j2 ;
+                    }
+
+            }
+        }
+        System.out.println(beautifulness);
+    }
+
+}
