481416dd100f0951fc9558c5934026df9b27de21
Mohammad Kasra Darini <71825544+KasraDarini@users.noreply.github.com>
2021-04-09 16:33:22 +0430

    Update BeautifulMatrix.java

--- BeautifulMatrix.java
+++ BeautifulMatrix.java
@@ -14,12 +14,12 @@ public class BeautifulMatrix{
         int j2 ;
         int i2  ;
         int beautifulness =0;
-        //calculate the total needed moves to reach the center .
+        //To calculate the total needed moves to reach the center .
         for(int i=0 ; i < 5 ; i++){
 
             for(int j = 0 ; j < 5 ; j++){
                     if(mtx[i][j] != 0){
-                         j2 =Math.abs( 2 -j);
+                         j2 =Math.abs(2-j);
                          i2 =Math.abs(2-i) ;
 
                          beautifulness =i2 + j2 ;
